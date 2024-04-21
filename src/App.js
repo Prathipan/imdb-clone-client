def9ad66456292f9cf027c1df610d5babddf1e73
Prathipan <prathipan24p@gmail.com>
2024-04-21 22:12:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,123 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { ThemeProvider } from '@emotion/react';
+import { AppBar, Button, Paper, Toolbar, createTheme } from '@mui/material';
+import { useState } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import AddMovie from './AddMovie';
+import Movie from './Movie';
+import AddActor from './AddActor';
+import AddProducer from './AddProducer';
+import EditMovies from './EditMovies';
 
 function App() {
+  const [mode, setMode] = useState("dark");
+  const navigate = useNavigate();
+
+  const theme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Paper className="paper" elevation={5} style={{ borderRadius: "0px", minHeight: "100vh" }}>
+        <AppBar className="nav-bar" position="static">
+          <Toolbar>
+            <Button
+              onClick={() => {
+                navigate("/");
+              }}
+              color="inherit"
+            >
+              <span className='logo'>IMDB</span>
+            </Button>
+            <Button
+              onClick={() => {
+                navigate("/");
+              }}
+              color="inherit"
+            >
+              All Movies
+            </Button>
+            <Button
+              onClick={() => {
+                navigate("/add-movies");
+              }}
+              color="inherit"
+            >
+              Add Movies
+            </Button>
+            <Button
+              onClick={() => {
+                navigate("/add-actor");
+              }}
+              color="inherit"
+            >
+              Add Actor
+            </Button>
+            <Button
+              onClick={() => {
+                navigate("/add-producer");
+              }}
+              color="inherit"
+            >
+              Add Producer
+            </Button>
+            <Button
+              style={{marginLeft : "auto"}}
+              startIcon = {mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+              onClick={() => {
+                setMode(mode === "light" ? "dark" : "light");
+              }}
+              color="inherit"
+            >
+              {mode === "light" ? "dark" : "light"} mode
+            </Button>
+          </Toolbar>
+        </AppBar>
+
+        <Routes>
+          <Route
+           exact
+            path="/"
+            element={<Movie  />}
+          />
+          <Route
+            path="/add-movies"
+            element={
+              <AddMovie  />
+            }
+          />
+          <Route
+            path="/add-actor"
+            element={
+              <AddActor  />
+            }
+          />
+          <Route
+            path="/add-producer"
+            element={
+              <AddProducer />
+            }
+          />
+          
+          <Route
+            path="movies/edit/:id"
+            element={
+              <EditMovies  />
+            }
+          />
+        </Routes>
+
+      </Paper>
+    </ThemeProvider>
   );
 }
+
+
 
 export default App;
---
+++ src/global.js
@@ -0,0 +1,2 @@
+
+export const backend_API = "http://localhost:4000"
